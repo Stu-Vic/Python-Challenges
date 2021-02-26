dcbbce4ca488cd94c6def7489906c4cec867b5a0
Stu <78116599+Stu-Vic@users.noreply.github.com>
2021-02-27 09:25:20 +1100

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,90 @@
-#PyPoll
-import csv
+# (PyPoll)
+
+# Import
 import os
+import csv
+
+# Variables
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+
+# Set Path 
+csvpath = os.path.join(r"C:/Users/stuar/bootcamp1/python_homework/Python-Challenges/PyPoll/Resources/election_data.csv")
+
+# Open & Read CSV File
+with open(csvpath, newline='') as csvfile:
+
+    # CSV Reader Specifies Delimiter 
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    # Read The Header Row First, Skip If  No Header
+    csv_header = next(csvfile)
+
+    # Read each row after the header
+    for row in csvreader:
+        
+        # calculate the total number of votes
+        total_votes += 1
+        
+        # Calculate the total number of votes for ea candidate
+        if (row[2] == "Khan"):
+            khan_votes += 1
+        elif (row[2] == "Correy"):
+            correy_votes += 1
+        elif (row[2] == "Li"):
+            li_votes += 1
+        else:
+            otooley_votes += 1
+            
+    # Calculate % won by each candidate
+    kahn_percent = khan_votes / total_votes
+    correy_percent = correy_votes / total_votes
+    li_percent = li_votes / total_votes
+    otooley_percent = otooley_votes / total_votes
+    
+    # Calculate winner
+    winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+
+    if winner == khan_votes:
+        winner_name = "Khan"
+    elif winner == correy_votes:
+        winner_name = "Correy"
+    elif winner == li_votes:
+        winner_name = "Li"
+    else:
+        winner_name = "O'Tooley" 
+
+# Print Analysis
+print(f"Election Results")
+print(f"---------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"---------------------------")
+print(f"Kahn: {kahn_percent:.3%}({khan_votes})")
+print(f"Correy: {correy_percent:.3%}({correy_votes})")
+print(f"Li: {li_percent:.3%}({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3%}({otooley_votes})")
+print(f"---------------------------")
+print(f"Winner: {winner_name}")
+print(f"---------------------------")
+
+# Specify file to write 
+output_file = os.path.join("C:/Users/stuar/bootcamp1/python_homework/Python-Challenges/PyPoll/analysis/election_result_data.txt")
+
+# Open File Using "Write" Mode. Specify The Variable To Hold The Contents
+with open(output_file, 'w',) as txtfile:
+
+# Write data
+    txtfile.write(f"Election Results\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Kahn: {kahn_percent:.3%}({khan_votes})\n")
+    txtfile.write(f"Correy: {correy_percent:.3%}({correy_votes})\n")
+    txtfile.write(f"Li: {li_percent:.3%}({li_votes})\n")
+    txtfile.write(f"O'Tooley: {otooley_percent:.3%}({otooley_votes})\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Winner: {winner_name}\n")
+    txtfile.write(f"---------------------------\n")
